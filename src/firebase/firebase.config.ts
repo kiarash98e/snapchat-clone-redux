694ba5727296ea528c5ed8a69d7c5725700ec780
kiarash98e <kiarashsafaei0@gmail.com>
2022-09-20 18:10:51 +0430

    firebase confige

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,6 +1,6 @@
 import { initializeApp  } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
+import { getStorage, } from 'firebase/storage'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 const config = {
