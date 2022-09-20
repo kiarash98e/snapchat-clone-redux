b5a63aae70cf238c28a51621f99acebeba811711
kiarash98e <kiarashsafaei0@gmail.com>
2022-09-20 18:08:57 +0430

    add webcamp capture page

--- src/components/webcamCapture/webcamCapture.tsx
+++ src/components/webcamCapture/webcamCapture.tsx
@@ -1,17 +1,32 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useRef } from 'react'
 import Webcam from 'react-webcam'
-
+import { MdRadioButtonUnchecked } from 'react-icons/md'
+import { useAppDispatch } from '../../redux/app/hooks'
+import { setCamraImage } from '../../redux/reducer/camraSlice'
+import { useNavigate } from 'react-router-dom'
 
 const webcamStyle = {
-    height:250,
-    width:400,
+    height:407,
+    width:274,
     facingMode:"user"
 }
 
 const WebcamCapture:React.FC = () => {
-    const webcamRef = useRef(null)
+    const webcamRef = useRef<any>()
+    const dispatch = useAppDispatch()
+    const history = useNavigate()
+
+    const capture = React.useCallback(() => {
+        const img = webcamRef!.current!.getScreenshot()
+        dispatch(setCamraImage(img))
+        history("/preview")
+    },[webcamRef])
+    
+    
     return (
-        <div className='webcamCapture'>
+        <div className='position-relative'>
             <Webcam 
                 audio={false}
                 ref={webcamRef}
@@ -19,6 +34,19 @@ const WebcamCapture:React.FC = () => {
                 height={webcamStyle.height}
                 width={webcamStyle.width}
                 videoConstraints={webcamStyle}
+            />
+            <MdRadioButtonUnchecked 
+                className=' text-white'
+                style={{    
+                    transform: "translate(-50%,-50%)",
+                    cursor: 'pointer',
+                    position: 'absolute',
+                    bottom: "0",
+                    left:"50%",
+                }}
+                fontWeight="bold"
+                fontSize={40}
+                onClick={capture}
             />
         </div>
     )
