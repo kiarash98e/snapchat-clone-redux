4cbb5facbee369a96acfb363cbb63e2208482a21
kiarash98e <kiarashsafaei0@gmail.com>
2022-09-20 18:12:10 +0430

    complate project

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { store } from './redux/app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en.json";
+TimeAgo.addDefaultLocale(en);
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
